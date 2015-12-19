30750d6abbb82371c219bb9efbbabc097f49eb6f
darkness_desktop <barabash97@gmail.com>
2015-12-19 22:32:52 +0100

    3 commit

--- www/engine/classes/database.class.php
+++ www/engine/classes/database.class.php
@@ -7,6 +7,32 @@
  */
 
 class Database {
+    private $mysqli;
+    private $config;
+    private $result;
+    public function __construct() {
+        $this->config = new Config();
+        $this->mysqli = new mysqli(
+                $this->config->db_host,
+                $this->config->db_user,
+                $this->config->db_password,
+                $this->config->db_name
+                );
+    }
+    
+    protected function query($query){
+        if(!empty($query)){
+           $this->result_set = $this->mysqli->query($query);
+        }
+    }
+    
+    public function __destruct() {
+        if(!empty($this->result_set)){
+            $this->result_set->close();
+        }
+        $this->mysqli->close();
+        
+    }
     
 }
 
